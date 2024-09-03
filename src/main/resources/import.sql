f88a1c3d4f3076448d98a0c0448166df51e117a6
Jorge Eduardo Cobo Ocampo <jorge.cobo01@uceva.edu.co>
2024-09-02 20:03:43 -0500

    Proyecto terminado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO celular (marca, modelo, celular) values ('Samsung','2022','3168236997');
-INSERT INTO celular (marca, modelo, celular) values ('Samsung','2022','3168236997');
-INSERT INTO celular (marca, modelo, celular) values ('Samsung','2022','3168236997');
-INSERT INTO celular (marca, modelo, celular) values ('Samsung','2022','3168236997');
-INSERT INTO celular (marca, modelo, celular) values ('Samsung','2022','3168236997');
+INSERT INTO celular (marca, modelo, celular) values ('Apple','2024','3188236997');
+INSERT INTO celular (marca, modelo, celular) values ('Xiaomi','2022','3178236997');
+INSERT INTO celular (marca, modelo, celular) values ('POCO','2021','3148236997');
+INSERT INTO celular (marca, modelo, celular) values ('ASUS','2020','3158236997');
